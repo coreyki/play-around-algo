1c3884726e644a26ecab0ff11fbed52ae8bf9761
corey <ck405517129@gmail.com>
2021-11-18 00:24:38 -0500

    add numberOfLongestIncreasingSubsequence.java

---
+++ weekSix/numberOfLongestIncreasingSubsequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int[] count = new int[nums.length];
+        Arrays.fill(dp, 1);
+        Arrays.fill(count, 1);
+        int maxLen = 0, ret = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    if (dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                        count[i] = count[j];
+                    } else if (dp[i] == dp[j] + 1) {
+                        count[i] += count[j];
+                    }
+                }
+            }
+            if (dp[i] == maxLen) {
+                ret += count[i];
+            } else if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                ret = count[i];
+            }
+        }
+        return ret;
+        //Time: O(n^2);
+        //Space: O(n);
+    }
+}
